d8f13c280c43ccba0c2452883d5ef8df79b5edb4
aek <konan.kouame@soprasteria.com>
2020-12-10 12:34:23 +0100

    init project

---
+++ src/main/java/com/tutorial/springbootmultitenancymongo/SpringBootMultiTenancyMongoApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.springbootmultitenancymongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiTenancyMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiTenancyMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutorial/springbootmultitenancymongo/SpringBootMultiTenancyMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springbootmultitenancymongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiTenancyMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
